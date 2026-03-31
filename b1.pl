30c89c9728d2ec510ed0f73064afd6d52520a3eb
SehanUdayanga <160522522+SehanUdayanga@users.noreply.github.com>
2026-03-31 14:28:04 +0530

    Add Prolog facts and rules for various operations3/32
    Added various Prolog facts and rules including member, factorial, nsum, and native dishes.

--- b1.pl
+++ b1.pl
@@ -1,12 +1,59 @@
-parent(john,mary).
-parent(anna,mary).
-parent(john,kamal).
+%Facts
+likes(john,mary).
+likes(sam,ann).
+likes(john,ann).
+likes(john,jenny).
+likes(peter,jenny).
+hates(john,peter).
 
-parent(sam,anna).
-parent(mary,supun).
-male(john).
-male(kamal).
+%Rules
+%if likes(X,Z) AND likes(Y,Z) then jelous(X,Y)
+jelous(X,Y) :-likes(X,Z),likes(Y,Z).
+%test cases
+%?jelous(john,peter)
+%?jelous(john,sam)
 
-grandpa(X,Y):-male(X),parent(X,Z),parent(Z,Y).
-sibling(X,Y):-male(Y),parent(Z,X), parent(Z,Y), X \= Y.
 
+%Prolog code to find max of two numbers
+max(X,Y,X) :- X>Y.
+max(X,Y,Y) :- Y>X.
+max(X,Y,X) :- X=Y.
+
+%Prolog code to find n!
+factorial(0,1).
+factorial(N,X) :- N1 is N-1, factorial(N1,Y), X is N * Y.
+%test cases
+%?factorial(5)
+
+%Prolog code to check a given element is a member of a list
+member(X,[X|_]).
+member(X,[_|T]) :-member(X,T).
+%test cases
+%?member(5,[7,4,5,3,2,1])
+%?member(9,[7,4,5,3,2,1])
+
+%Write a Prolog program to calculate sum on n numbers 1+2+3+...n
+
+
+nsum(0,0).
+nsum(N,SUM) :- N>0, N1 is N-1, nsum(N1,SUM1), SUM is N + SUM1.
+
+%Find alternative for National Dish
+%native_dish(Dish, Country)
+native_dish(kottu, sri_lanka).
+native_dish(rice_and_curry, sri_lanka).
+native_dish(hopper, sri_lanka).
+
+native_dish(rice_and_curry, india).
+
+native_dish(pizza, italy).
+native_dish(pasta, italy).
+
+native_dish(burger, usa).
+native_dish(apple_pie, usa).
+
+% official_national_dish(Dish, Country)
+% Note: This dish MUST also be a native_dish of that country.
+official_national_dish(rice_and_curry, india).
+official_national_dish(pizza, italy).
+official_national_dish(burger, usa).
