3e17b6ab6d2df1ee7b0b9d36ff268cf869368c34
SehanUdayanga <160522522+SehanUdayanga@users.noreply.github.com>
2026-04-06 03:03:24 +0530

    Add length and concat functions to list.pl

--- list.pl
+++ list.pl
@@ -13,18 +13,30 @@ true
 
 ?reverse([1,2,3,4],L)
 L=[4,3,2,1]
-
+-------------------------------------------------------
 ? length([1,2,3,4],L)
 L=4
 
+length_list([],0).
+
+length_list([_|T],N):-
+    length_list(T,N1),
+    N is N1 + 1.
+---------------------------------------------
 append([1,2],[3,4],L)
 L[1,2,3,4]
 
+concat 
+
+concat([],L,L).
+
+concat([H|T],L,[H|R]):-
+    concat(T,L,R).
 
 using the recursive part 
 is_member(X,[X|_]).
 is_member(X,[_|T]):-is_member(X,T).
-
+----------------------------------------------------
 ?is_member(2,[1,2,3,4])  =>true
 in here,
 1 st checj is 2 is first elemt od list if no 
@@ -32,7 +44,7 @@ then go to the second line and check The tail value and in the tail value first 
 this repet the list is end or eqal numbe gets
 
 
-
+-------------------------------------------------------
 get the sum of list value 
 
 sum_list([],0).
